8b88ee8f0f2bf0aa090cf8bcbc9c8684dab12600
FadeevaDasha <genkovna1999@gmail.com>
2025-09-12 19:18:06 +0300

    Добавила allure к тестам

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -2,6 +2,8 @@ from pyexpat.errors import messages
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+import allure
+
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
@@ -9,5 +11,9 @@ class BasePage:
     def find_element(self, locator, time=5):
         return WebDriverWait(self.driver, time).until(expected_conditions.visibility_of_element_located(locator), message=f'Не удалось найти элемент {locator}')
 
+    @allure.step('Открываем страницу')
     def get_url(self, url):
         return self.driver.get(url)
+
+    def attach_screenshot(self):
+        allure.attach(self.driver.get_screenshot_as_png(), 'скриншот', allure.attachment_type.PNG)
